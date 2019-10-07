6e1230abad323837fa17fce8986276d27fe2f7e9
NikitaPetrovsiy <nikpetrovskiy23@gmail.com>
2019-10-07 13:12:04 +0300

    Connected to the project mongoDB

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+
 const PORT = process.env.PORT || 3000;
+const app = express();
 
+async function start() {
+  try {
+      mongoose.connect('mongodb+srv://Nikita:1561@cluster0-fprxz.mongodb.net/todos', {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          useFindAndModify: false
+      });
+      app.listen(PORT, () => {
+          console.log('Server has been started');
+      });
+  }  catch (e) {
+      console.log(e);
+  };
+};
 
-app.listen(PORT, () => {
-    console.log('Server has been started');
-});
+start();
